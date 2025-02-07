3f31e78ae471e95ecfcd21e5a819eb9e7f73480c
Jun Quiletorio <44885253+alfredoquiletorio@users.noreply.github.com>
2025-02-07 08:51:54 +0000

    trylast

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,12 @@ resource "aws_instance" "flask_app" {
     private_key = filebase64(var.private_key_path) # ✅ Fix: Use base64 function
     host        = self.public_ip
   }
+}
 
-  provisioner "file" {
+provisioner "file" {
     source      = "../ansible"
     destination = "/home/ec2-user/ansible"
   }
-}
 
 output "ec2_public_ip" {
   value = aws_instance.flask_app.public_ip
